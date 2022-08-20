a3e394b7ab6f466d82df61a970b3f8bbe5071862
le-ar <andreiklauzer@gmail.com>
2022-08-20 14:59:03 +0300

    GetClientsResponseDtoZ

--- src/dto/GetClients.ts
+++ src/dto/GetClients.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const GetClientsResponseDtoZ = z.array(
   z.object({
     uid: z.string().uuid(),
-    userProfole: z.object({
+    userProfile: z.object({
       firstName: z.string(),
       lastName: z.string(),
       middleName: z.string().optional(),
