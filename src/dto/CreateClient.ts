de58350960fbbbc9217bbb514f54cb13abf47046
le-ar <andreiklauzer@gmail.com>
2022-08-23 10:11:27 +0300

    fixed CreateClientDtoZ phone

--- src/dto/CreateClient.ts
+++ src/dto/CreateClient.ts
@@ -5,12 +5,15 @@ export const CreateClientDtoZ = z.object({
   lastName: z.string().min(1),
   firstName: z.string().min(1),
   middleName: z.string().min(1).optional(),
-  phone: z.string().refine(
-    (val) => phone(val).isValid,
-    (val) => ({
-      message: `${val} is not a valid phone number`,
-    })
-  ).transform(val => phone(val).phoneNumber),
+  phone: z
+    .string()
+    .refine(
+      (val) => phone(val).isValid,
+      (val) => ({
+        message: `${val} is not a valid phone number`,
+      })
+    )
+    .transform<string>((val) => phone(val).phoneNumber!),
 });
 
 export const CreateClientResponseDtoZ = z.object({
