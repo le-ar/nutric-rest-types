10ac54d7ee3171fb45e242306ca1ea7837d1aadf
le-ar <andreiklauzer@gmail.com>
2022-09-04 11:40:20 +0300

    updated get me

--- src/dto/GetMe.ts
+++ src/dto/GetMe.ts
@@ -2,4 +2,7 @@ import { z } from "zod";
 import { ProductZ } from "../models/Product";
 import { UserZ } from "../models/User";
 
-export const GetMeResponseDtoZ = z.object({ user: UserZ });
+export const GetMeResponseDtoZ = z.object({
+  user: UserZ,
+  permissions: z.array(z.string()),
+});
