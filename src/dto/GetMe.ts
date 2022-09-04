8dcf3fb4d52f7c23046c62d47e44103f5bd01e82
le-ar <andreiklauzer@gmail.com>
2022-09-04 11:43:53 +0300

    updated get me

--- src/dto/GetMe.ts
+++ src/dto/GetMe.ts
@@ -4,5 +4,13 @@ import { UserZ } from "../models/User";
 
 export const GetMeResponseDtoZ = z.object({
   user: UserZ,
+  profile: z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    middleName: z.string().nullable().optional(),
+  }),
+  contacts: z.object({
+    phone: z.string(),
+  }),
   permissions: z.array(z.string()),
 });
